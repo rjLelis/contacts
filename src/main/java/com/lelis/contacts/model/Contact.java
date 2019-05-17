c4a8fdbb414b1b3532977f424c00fd2d4cd01893
rjLelis <renatojlelis@gmail.com>
2019-05-17 11:06:11 -0300

    Indent Contact.java

--- src/main/java/com/lelis/contacts/model/Contact.java
+++ src/main/java/com/lelis/contacts/model/Contact.java
@@ -12,51 +12,57 @@ import javax.validation.constraints.Size;
 @Entity
 public class Contact {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Size(min = 3, max = 50)
-    @NotEmpty
-    private String name;
+	@Size(min = 3, max = 50)
+	@NotEmpty
+	private String name;
 
-    @Email
-    @NotEmpty
-    private String email;
+	@Email
+	@NotEmpty
+	private String email;
 
-    @Size(max = 11)
-    @NotBlank
-    private String phone;
+	@Size(max = 11)
+	@NotBlank
+	private String phone;
 
+	public Long getId() {
+		return id;
+	}
 
-    public String getName(){
-        return name;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getEmail(){
-        return email;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public String getPhone(){
-        return phone;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
+	public String getPhone() {
+		return phone;
+	}
 
-    @Override
-    public String toString() {
-        return "[Name="+ name + ", Email="+ email + ", Phone=" + phone +']';
-    }
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
+	@Override
+	public String toString() {
+		return "[Name=" + name + ", Email=" + email + ", Phone=" + phone + ']';
+	}
 
 }
